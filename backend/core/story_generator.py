92cacc22aa86b8b73cfea3eea12e1236b515e56e
RRRuby1225 <wyr2115@gmail.com>
2025-08-25 17:41:30 +0800

    update file

--- backend/core/story_generator.py
+++ backend/core/story_generator.py
@@ -5,11 +5,22 @@ from langchain_core.output_parsers import PydanticOutputParser
 from core.prompts import STORY_PROMPT
 from core.models import StoryLLMResponse, StoryNodeLLM
 from core.config import settings
+import os
 
 
 class StoryGenerator:
     @classmethod
     def _get_llm(cls):
+        #部署环境中获取openai_api_key和service_url
+        openai_api_key = os.getenv("CHOREO_OPENAI_CONNECTION_OPENAI_API_KEY")
+        service_url = os.getenv("CHOREO_OPENAI_CONNECTION_SERVICEURL")
+        if openai_api_key and service_url:
+            return ChatOpenAI(
+                model="gpt-3.5-turbo:free",
+                api_key=openai_api_key,
+                base_url=service_url
+            )
+
         return ChatOpenAI(
             model="gpt-3.5-turbo:free",
             api_key=settings.POIXE_API_KEY,
